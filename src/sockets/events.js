6184448b9f7fd75406bcfcd2e78dffe2e0fd34b3
tusharojha <tusharojha2001@gmail.com>
2020-04-24 00:39:30 +0530

    room join added

--- src/sockets/events.js
+++ src/sockets/events.js
@@ -1,4 +1,11 @@
-module.exports = {
-    CREATE_ROOM: "create room",
-    CONNECT_GAME: "connect game"
+const listenrs = {
+  CREATE_ROOM: "create room",
+  CONNECT_GAME: "connect game",
+  JOIN_ROOM: "join room",
 };
+
+const emits = {
+  EXIT_ROOM: "exit user",
+};
+
+module.exports = { ...listenrs, ...emits };
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -2,7 +2,7 @@
 const socketIO = require("socket.io");
 
 // Project Imports
-const { CREATE_ROOM, CONNECT_GAME } = require("./events");
+const { CREATE_ROOM, JOIN_ROOM, EXIT_USER } = require("./events");
 const { authenticateSocket } = require("./../middlewares/authenticate");
 
 const createRoomName = () =>
@@ -16,7 +16,20 @@ const connectSockets = (server) => {
     console.log("New User Connected ");
 
     // User Disconnected
-    socket.on("disconnect", () => console.log("User Disconnected"));
+    socket.on("disconnect", () => {
+      if (socket.user != null) console.log(`${socket.user.name} disconnected`);
+      else console.log("User Disconnected");
+
+    //   if (socket.rooms != null) {
+    //     var rooms = socket.rooms;
+    //     // remove the default room from the list
+    //     rooms.splice(0, 1);
+
+    //     rooms.forEach((room) => {
+    //       socket.to(room).emit(EXIT_USER, { name: user.name, id: user._id });
+    //     });
+    //   }
+    });
 
     // CREATE_ROOM event added with authentication
     socket.on(CREATE_ROOM, (data, callback) => {
@@ -30,6 +43,7 @@ const connectSockets = (server) => {
             socket.join(randomRoom, (err) => {
               if (!err) {
                 console.log("ROOM CREATED: ", randomRoom);
+                socket.user = user;
                 // console.log(io.nsps["/"].adapter.rooms[randomRoom].length);
                 callback({ status: 201, message: "New room created" });
               } else {
@@ -38,10 +52,44 @@ const connectSockets = (server) => {
               }
             });
           } else {
-            console.log({
+            callback({
               status: 501,
               message: "Unable to create room, please try again.",
             });
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+          callback(e);
+        });
+    });
+
+    // JOIN_ROOM event
+    socket.on(JOIN_ROOM, (data, callback) => {
+      authenticateSocket(data)
+        .then((user) => {
+          if (data.roomName != null) var roomName = data.roomName.toString();
+          else callback({ status: 400, message: "Room Name is required" });
+          if (io.nsps["/"].adapter.rooms[roomName] != undefined) {
+            if (io.nsps["/"].adapter.rooms[roomName].length < 5) {
+              socket.join(roomName, (err) => {
+                if (!err) {
+                  console.log("ROOM JOINED by ", user.name);
+                  socket.user = user;
+                  socket.to(roomName).broadcast.emit(`${user.name} Joined`);
+                  callback({
+                    status: 200,
+                    message: "Room Joined successfully",
+                  });
+                }
+              });
+            } else {
+              // TODO: change console.log to callback
+              console.log({ status: 403, message: "Room is already full" });
+            }
+          } else {
+            // TODO: change console.log to callback
+            console.log({ status: 400, message: "Room does not exists" });
           }
         })
         .catch((e) => {
