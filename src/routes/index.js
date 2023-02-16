ee71747bc6d4aae68e15a13fcd163e4ed34cec05
Tushar Ojha <tusharojha2001@gmail.com>
2023-02-16 10:59:01 +0530

    added comments for router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const express = require("express")
 // Router Imports
 const user = require("./user")
 
+// Setting up router for express.
 const router = express.Router()
 
+// Adding router middleware.
 router.use('/user', user)
 
 module.exports = router
